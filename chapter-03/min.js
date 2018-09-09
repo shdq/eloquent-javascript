ce8074069080343d9d0438a728334be6475ab2ea
Sergei Cherniaev <sergei@cherniaev.com>
2018-09-09 22:07:58 +0300

    Min exercise added

---
+++ chapter-03/min.js
@@ -0,0 +1,12 @@
+function min(one, two) {
+  if(typeof one != 'number' || typeof two != 'number') {
+    return Error('One or both arguments are not a numbers');
+  }
+  if (one === two) {
+    return Error('Numbers are equal');
+  }
+  if(one < two ) {
+    return one;
+  }
+  return two;
+}
