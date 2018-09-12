c48b6365245a98d120fca14bc50c149633367b89
Sergei Cherniaev <sergei@cherniaev.com>
2018-09-12 09:46:13 +0300

    Recursion exercise added

---
+++ chapter-03/recursion.js
@@ -0,0 +1,11 @@
+function isEven(number) {
+  if(number === 0) {
+    return true;
+  } else if(number === 1) {
+    return false;
+  } else  if(number < 0) {
+    isEven(number + 2);
+  } else {
+    isEven(number - 2);
+  }
+}
