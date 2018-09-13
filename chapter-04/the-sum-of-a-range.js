711f3cd0d8b759001d00350f3a54f553078b2030
Sergei Cherniaev <sergei@cherniaev.com>
2018-09-13 15:45:14 +0300

    The sum of a range exercise added

---
+++ chapter-04/the-sum-of-a-range.js
@@ -0,0 +1,24 @@
+function range(start, end, step = 1) {
+  step = Math.abs(step);
+  let result = [];
+
+  if(start > end) {
+    for(start; start >= end; start -= step) {
+      result.push(start)
+    }
+    return result;
+  }
+
+  for(start; start <= end; start += step) {
+    result.push(start)
+  }
+  return result;
+}
+
+function sum(numbers) {
+  let total = 0;
+  for (number of numbers) {
+    total += number;
+  }
+  return total;
+}
