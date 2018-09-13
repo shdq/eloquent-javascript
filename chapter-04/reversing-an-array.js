f43f3861ed216105d1e6263ee35d81b903619c11
Sergei Cherniaev <sergei@cherniaev.com>
2018-09-13 17:31:21 +0300

    Reversing an array exercise added

---
+++ chapter-04/reversing-an-array.js
@@ -0,0 +1,17 @@
+function reverseArray(array) {
+  let resultArray = [];
+  for(let element of array) {
+    resultArray.unshift(element);
+  }
+  return resultArray;
+}
+
+function reverseArrayInPlace(array) {
+  let value;
+  for(let i = 0, j = array.length - 1; i < j; i++, j--) {
+    value = array[i];
+    array[i] = array[j];
+    array[j] = value;
+  }
+  return array;
+}
