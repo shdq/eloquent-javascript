6a0de74561239c53f64aec75c6dbfcb5d49dc36b
Sergei Cherniaev <sergei@cherniaev.com>
2018-09-15 11:46:12 +0300

    arrayToList refactored with prepend function

--- chapter-04/a-list.js
+++ chapter-04/a-list.js
@@ -1,18 +1,22 @@
+// Array to List
+
 function arrayToList(array) {
   let list = {};
-  let memory = {
-    value: array[array.length - 1],
-    rest: null
-  }
-  for(let i = array.length - 2; i >= 0; i--) {
-    list = {
-      value: array[i],
-      rest: memory
-    }
-    memory = list; 
+  for(let i = array.length - 1; i >= 0; i--) {
+    list = prepend(array[i], list);
   }
   return list;
 }
+
+function prepend(element, list) {
+  let newList = {
+    value: element,
+    rest: list.rest === undefined ? null : list
+  }
+  return newList;
+}
+
+// List to array
 
 function popList(list) {
   newList = list.rest;
