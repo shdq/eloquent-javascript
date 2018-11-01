0734364932158bee4ab5450ecf2e1b29f2aa3c45
Sergei Cherniaev <sergei@cherniaev.com>
2018-11-01 15:36:33 +0300

    you own loop exercise added

---
+++ chapter-05/your-own-loop.js
@@ -0,0 +1,12 @@
+function loop(value, testFunc, updateFunc, bodyFunc) {
+  while(testFunc(value)) {
+    bodyFunc(value);
+    value = updateFunc(value);
+  }
+  return;
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
