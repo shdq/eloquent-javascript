7ebb907d90b0cff72fa1c76d88927b4bbca876f6
Sergei Cherniaev <sergei@cherniaev.com>
2018-12-24 13:04:52 +0300

    dominant writing direction exercise solved

--- chapter-05/dominant-writing-direction.js
+++ chapter-05/dominant-writing-direction.js
@@ -1,3 +1,5 @@
+const  SCRIPTS = require('./data/scripts');
+
 function characterScript(code) {
   for (let script of SCRIPTS) {
     if (script.ranges.some(([from, to]) => {
@@ -24,11 +26,19 @@ function countBy(items, groupName) {
 }
 
 function dominantDirection(text) {
-  // Your code here.
+  let directions = countBy(text, char => {
+    let direction = characterScript(char.codePointAt(0));
+    return direction ? direction.direction : "none";
+  }).filter(({name}) => name != "none");
+
+  let total = directions.reduce((n, {count}) => n + count, 0);
+  if (total == 0) return "No caracters with particular direction found";
+
+  return directions.reduce((a, b) => a.count > b.count ? a : b).name;
+
 }
 
 console.log(dominantDirection("Hello!"));
 // → ltr
 console.log(dominantDirection("Hey, مساء الخير"));
 // → rtl
-
