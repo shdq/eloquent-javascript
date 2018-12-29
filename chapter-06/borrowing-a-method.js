be2867576be622a26ae01c9c7a11611394428d26
Sergei Cherniaev <sergei@cherniaev.com>
2018-12-29 14:23:03 +0300

    borrowing a method exercise added

---
+++ chapter-06/borrowing-a-method.js
@@ -0,0 +1,7 @@
+let map = {one: true, two: true, hasOwnProperty: true};
+
+// Option one
+console.log({}.hasOwnProperty.call(map, 'one'));
+// Option two
+console.log(Object.prototype.hasOwnProperty.call(map, 'one'));
+// → true
