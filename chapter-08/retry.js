8c8979be9a408ad63bb2a2d0496de56d0b22bd45
Sergei Cherniaev <sergei@cherniaev.com>
2019-02-10 21:17:41 +0300

    initial commit

---
+++ chapter-08/retry.js
@@ -0,0 +1,16 @@
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) {
+  // Your code here.
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
