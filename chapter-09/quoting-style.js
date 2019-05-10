60a7206a7769c9699cc1738c2a0dbe5f6f77043f
Sergei Cherniaev <sergei@cherniaev.com>
2019-05-10 21:50:10 +0300

    regexp added

--- chapter-09/quoting-style.js
+++ chapter-09/quoting-style.js
@@ -1,4 +1,4 @@
 let text = "'I'm the cook,' he said, 'it's my job.'";
 // Change this call.
-console.log(text.replace(/A/g, "B"));
+console.log(text.replace(/(\W|^)'|'($)/g, '$1"$2'));
 // → "I'm the cook," he said, "it's my job."
